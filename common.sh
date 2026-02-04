f80e2f5740a26e3d2131ec9d2b9eca75ac8e76fd
soumya <korlagani.soumya@gmail.com>
2026-02-04 20:09:59 +0530

    roboshop-common

--- common.sh
+++ common.sh
@@ -58,6 +58,15 @@ java_setup(){
     VALIDATE $? "Moving and Renaming $app_name"
 }
 
+python_setup(){
+    dnf install python3 gcc python3-devel -y &>>$LOGS_FILE
+    VALIDATE $? "Installing Python"
+
+    cd /app 
+    pip3 install -r requirements.txt &>>$LOGS_FILE
+    VALIDATE $? "Installing dependencies"
+}
+
 
 app_setup(){
     # creating system user
